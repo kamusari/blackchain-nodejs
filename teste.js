a64db12c40dfef9e17b7aa64f6ea3d548dd2ef5f
Kamusari <lucker.de.souza@gmail.com>
2021-07-04 18:08:15 -0300

    add crypto-js

--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-const Block = require('./block');
+const Block = require('./block');genesis
 
 const block = new Block('74517', '78454ashAHSU4', '4521587ASHDH35', '100');
 console.log(block.toString());
