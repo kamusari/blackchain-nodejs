f47e423ef4a4fb1125c86cb3d872029e1ed842c9
Kamusari <lucker.de.souza@gmail.com>
2021-07-05 17:15:42 -0300

    create class p2pserver and function listen

--- app/p2pSever.js
+++ app/p2pSever.js
@@ -1,1 +1,16 @@
 const websocket = require('ws');
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.slipt(',') : [];
+
+class P2pSever {
+  constructor(blockchain){
+    this.blockchain = blockchain;
+    this.socket = [];
+  }
+
+  listen(){
+    const server = new websocket.Server({port: P2P_PORT});
+  }
+}
+
+module.exports = P2pSever;
