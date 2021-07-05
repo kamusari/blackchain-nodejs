fcdd95035c7a33c5a0c201c0269f4c9cfe6db8ca
Kamusari <lucker.de.souza@gmail.com>
2021-07-05 17:23:04 -0300

    create function connectToPeers

--- app/p2pSever.js
+++ app/p2pSever.js
@@ -10,7 +10,25 @@ class P2pSever {
 
   listen(){
     const server = new websocket.Server({port: P2P_PORT});
+    server.on('connection', socket => this.connectSocket(socket));
+    
+    this.connectToPeers();
+
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
   }
+
+  connectToPeers(){
+    peers.forEach(peer => {
+      const socket = new websocket(peer);
+      socket.on('open', () => {this.connectSocket(socket)})
+    });
+  }
+
+  connectSocket(socket){
+    this.socket.push(socket);
+    console.log('socket connected');
+  }
+
 }
 
 module.exports = P2pSever;
