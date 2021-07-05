961e5cb09797d4bb206bac3a87c5e80af6eebbf3
Kamusari <lucker.de.souza@gmail.com>
2021-07-05 17:37:14 -0300

    start server p2p

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const P2pServer = require('./p2pSever');
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
 
 app.use(express.json());
 app.get('/blocks', (req, res) => {
@@ -18,3 +21,4 @@ app.post('/mine', (req, res) => {
 })
 
 app.listen(HTTP_PORT, () => console.log(`listenin on port ${HTTP_PORT}`));
+p2pServer.listen()
--- app/p2pSever.js
+++ app/p2pSever.js
@@ -1,6 +1,6 @@
 const websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
-const peers = process.env.PEERS ? process.env.PEERS.slipt(',') : [];
+const peers = process.env.PEERS ? process.env.PEERS.split (',') : [];
 
 class P2pSever {
   constructor(blockchain){
